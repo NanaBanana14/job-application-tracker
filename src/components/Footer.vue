3e56fd527d8e667bffd698f6493117f847ee9751
Ika Triyana <134359016+NanaBanana14@users.noreply.github.com>
2025-06-29 15:02:58 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="py-6 text-center text-sm text-white/60">
-    &copy; {{ new Date().getFullYear() }} Job Application Tracker — Dibuat dengan ❤️ oleh Kamu
+    &copy; {{ new Date().getFullYear() }} Job Application Tracker — Nana
   </footer>
 </template>
 
